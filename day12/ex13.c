d8c46632d2096e0d1f0a59f32b009c49cef9df30
gbox3d <gbox3d@gmail.com>
2020-08-11 17:16:29 +0900

    ...

--- day12/ex13.c
+++ day12/ex13.c
@@ -11,10 +11,17 @@ SDL_Window *g_pWindow;
 SDL_Renderer *g_pRenderer;
 SDL_Texture *g_pTileSet;
 
+Uint16 g_worldMap_Layer_1[64];
+
 Uint16 g_nSelectTileIndex = 0;
 
 int main(int argc, char *argv[])
 {
+  for (int i = 0; i < 64; i++)
+  {
+    g_worldMap_Layer_1[i] = -1;
+  }
+
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
   {
     printf("error initializing SDL: %s\n", SDL_GetError());
@@ -48,16 +55,29 @@ int main(int argc, char *argv[])
     SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 1);
     SDL_RenderClear(g_pRenderer);
 
-    //select tile 
-    putTile(g_pRenderer,g_pTileSet,15,0,g_nSelectTileIndex);
+    //select tile
+    putTile(g_pRenderer, g_pTileSet, 15, 0, g_nSelectTileIndex);
 
     //440,100
     {
-      //팔래트 랜디링 
+      //팔래트 랜디링
       SDL_Rect _dstRt = {440, 100, 48 * 4, 72 * 4};
       SDL_RenderCopy(g_pRenderer, g_pTileSet, NULL, &_dstRt);
-      SDL_RenderPresent(g_pRenderer);
     }
+
+    //render world map
+    {
+      for (int i = 0; i < 64; i++)
+      {
+        int _index = g_worldMap_Layer_1[i];
+        if (_index != -1)
+        {
+          putTile(g_pRenderer, g_pTileSet, i % 8, i / 8, _index);
+        }
+      }
+    }
+
+    SDL_RenderPresent(g_pRenderer);
 
     SDL_Event _event;
     while (SDL_PollEvent(&_event))
@@ -71,17 +91,48 @@ int main(int argc, char *argv[])
       break;
       case SDL_MOUSEBUTTONDOWN:
       {
-        printf("%8d\r",_event.button.button);
-        if(_event.button.button == 1) //마우스 좌클릭
+        printf("%8d\r", _event.button.button);
+        if (_event.button.button == 1) //마우스 좌클릭
         {
-           int _x = (_event.motion.x - 440) / 32;
-           int _y = (_event.motion.y - 100) / 32;
+          //팔래트처리
+          {
+            int _x = (_event.motion.x - 440) / 32;
+            int _y = (_event.motion.y - 100) / 32;
 
-           
-           
-           printf("%4d%4d\r",_x,_y);
+            if ((_x >= 0 && _y >= 0) && (_x < 6 && _y < 9))
+            {
+              g_nSelectTileIndex = _y * 6 + _x;
+            }
+
+            // printf("%4d%4d\r", _x, _y);
+          }
+          //월드멥처리
+          {
+            int _x = (_event.motion.x) / 32;
+            int _y = (_event.motion.y) / 32;
+
+            if (_x < 8 && _y < 8)
+            {
+              int _tileIndex = _y * 8 + _x;
+              // printf("%8d \r",_tileIndex);
+              g_worldMap_Layer_1[_tileIndex] = g_nSelectTileIndex;
+              printf("%4d%4d%4d\r", _x, _y, _tileIndex);
+            }
+          }
         }
-        
+        else if (_event.button.button == 3) //마우스 우클릭 
+        {
+          {
+            int _x = (_event.motion.x) / 32;
+            int _y = (_event.motion.y) / 32;
+
+            if (_x < 8 && _y < 8)
+            {
+              int _tileIndex = _y * 8 + _x;
+              g_worldMap_Layer_1[_tileIndex] = -1;              
+            }
+          }
+        }
       }
       break;
       case SDL_KEYDOWN:
