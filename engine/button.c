7ffbf66b3431f6e700a78455861fd14601568c10
gbox3d <gbox3d@gmail.com>
2020-08-12 15:46:04 +0900

    ....

--- engine/button.c
+++ engine/button.c
@@ -20,14 +20,21 @@ void Button_Init(S_BUTTON *pBtn, int x, int y, int w, int h, Uint16 btnID)
 void Button_Render(S_BUTTON *pBtn, SDL_Renderer *pRender)
 {
   
+  //반투명 모드 활성화
+  SDL_SetRenderDrawBlendMode(pRender,SDL_BLENDMODE_BLEND);
+  
+  SDL_SetRenderDrawColor(pRender, 255, 255, 255, 255);
+  SDL_RenderDrawRect(pRender, &pBtn->m_Rect);
+
+
   SDL_SetRenderDrawColor(pRender, pBtn->m_fillColor.r, pBtn->m_fillColor.g, pBtn->m_fillColor.b, 
     pBtn->m_fillColor.a
   );
   SDL_RenderFillRect(pRender, &pBtn->m_Rect);
 
-  SDL_SetRenderDrawColor(pRender, 255, 255, 255, 255);
-  SDL_RenderDrawRect(pRender, &pBtn->m_Rect);
 
+  //반투명 모드 비 활성화
+  SDL_SetRenderDrawBlendMode(pRender,SDL_BLENDMODE_NONE);
 
 }
 
@@ -43,7 +50,7 @@ void Button_DoEvent(S_BUTTON *pBtn, SDL_Event *pEvt)
         pBtn->m_fillColor.r = 255;
         pBtn->m_fillColor.g = 0;
         pBtn->m_fillColor.b = 0;
-        pBtn->m_fillColor.a = 255;
+        pBtn->m_fillColor.a = 0x80;
 
         pBtn->m_nFSM = 1;
       }
@@ -61,7 +68,7 @@ void Button_DoEvent(S_BUTTON *pBtn, SDL_Event *pEvt)
         pBtn->m_fillColor.g = 0;
         pBtn->m_fillColor.b = 0;
         pBtn->m_fillColor.a = 0;
-        
+
         pBtn->m_nFSM = 0;
       }
       break;    
