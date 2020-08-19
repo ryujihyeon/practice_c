8b68a17a51b0d2cd5d98c5326751048a0f868965
gbox3d <gbox3d@gmail.com>
2020-08-19 10:59:05 +0900

    ....

--- day18/maptool_exam.c
+++ day18/maptool_exam.c
@@ -1,5 +1,6 @@
 #include "../engine/core.h"
 #include "../engine/tile.h"
+#include "cmd_parser.h"
 
 tDE_S_Core *g_pEngineCore;
 SDL_Texture *g_pTileSet;
@@ -11,35 +12,55 @@ int main(int argc, char *argv[])
   for (int i = 0; i < 64; i++)
   {
     g_worldMap_Layer_1[i] = -1;
-  }  
-  g_pEngineCore = tDE_setup_1("maptool",640,480,0);
-  g_pTileSet = tDE_util_loadTexture(g_pEngineCore,"../res/dungeon.png");
+  }
+  g_pEngineCore = tDE_setup_1("maptool", 640, 480, 0);
+  g_pTileSet = tDE_util_loadTexture(g_pEngineCore, "../res/dungeon.png");
 
   SDL_bool bLoop = SDL_TRUE;
   while (bLoop)
   {
-    SDL_Renderer *pRender = g_pEngineCore->m_pRender; 
+    SDL_Renderer *pRender = g_pEngineCore->m_pRender;
     SDL_SetRenderDrawColor(pRender, 0, 0, 0, 1);
     SDL_RenderClear(pRender);
 
     //select tile
-    putTile(pRender, g_pTileSet, 15, 0, g_nSelectTileIndex,16,5,2);
+    putTile(pRender, g_pTileSet, 15, 0, g_nSelectTileIndex, 16, 5, 2);
 
     //440,100
     {
       //팔래트 랜디링
-      SDL_Rect _dstRt = {440, 100, 80 * 4, 80 * 4};
+      SDL_SetRenderDrawColor(pRender, 0xff, 0xff, 0xff, 0xff);
+      SDL_Rect _rtTemp;
+      _rtTemp.x = 440;
+      _rtTemp.y = 0;
+      _rtTemp.w = 160;
+      _rtTemp.h = 350;
+      SDL_RenderDrawRect(pRender, &_rtTemp);
+
+      SDL_Rect _dstRt = {440, 100, 80 * 2, 80 * 2};
       SDL_RenderCopy(pRender, g_pTileSet, NULL, &_dstRt);
     }
 
     //render world map
     {
+      SDL_SetRenderDrawColor(pRender, 0xff, 0xff, 0xff, 0xff);
+      // SDL_RenderDrawLine(pRender,0,0,256,0);
+      // SDL_RenderDrawLine(pRender,256,0,256,256);
+      // SDL_RenderDrawLine(pRender,256,256,0,256);
+      // SDL_RenderDrawLine(pRender,0,256,0,0);
+      SDL_Rect _rtTemp;
+      _rtTemp.x = 0;
+      _rtTemp.y = 0;
+      _rtTemp.w = 256;
+      _rtTemp.h = 256;
+      SDL_RenderDrawRect(pRender, &_rtTemp);
+
       for (int i = 0; i < 64; i++)
       {
         Sint16 _index = g_worldMap_Layer_1[i];
         if (_index != -1)
         {
-          putTile(pRender, g_pTileSet, i % 8, i / 8, _index,16,5,2);
+          putTile(pRender, g_pTileSet, i % 8, i / 8, _index, 16, 5, 2);
         }
       }
     }
@@ -49,6 +70,9 @@ int main(int argc, char *argv[])
     SDL_Event _event;
     while (SDL_PollEvent(&_event))
     {
+      static char szBuf[64];
+      static Uint16 nInputFSM = 0;
+
       switch (_event.type)
       {
       case SDL_MOUSEMOTION:
@@ -87,7 +111,7 @@ int main(int argc, char *argv[])
             }
           }
         }
-        else if (_event.button.button == 3) //마우스 우클릭 
+        else if (_event.button.button == 3) //마우스 우클릭
         {
           {
             int _x = (_event.motion.x) / 32;
@@ -96,15 +120,62 @@ int main(int argc, char *argv[])
             if (_x < 8 && _y < 8)
             {
               int _tileIndex = _y * 8 + _x;
-              g_worldMap_Layer_1[_tileIndex] = -1;              
+              g_worldMap_Layer_1[_tileIndex] = -1;
             }
           }
         }
       }
       break;
       case SDL_KEYDOWN:
-        printf("%d \n", _event.key.keysym.scancode);
+        //printf("%d \n", _event.key.keysym.scancode);
+        {          
+          switch (nInputFSM)
+          {
+          case 0: //대기상태
+          {
+            if (_event.key.keysym.sym == SDLK_RETURN)
+            {
+              printf("input cmd => \n");
+              nInputFSM = 1; //입력 상태로 전이
+            }
+          }
+          break;
+          case 1: //입력 상태
+          {
+            
+            if(_event.key.keysym.sym == SDLK_RETURN)
+            {
+              nInputFSM = 0; //대기 상태로 전이 
+              // printf("\n%s\n",szBuf);
+              //cmd parser
+              {
+                parseCmd(szBuf);
+              }
+
+              szBuf[0] = 0x00;  //문자열 클리어 
+            }            
+            else if(_event.key.keysym.sym == SDLK_BACKSPACE)
+            {
+              int _len = strlen(szBuf);
+              szBuf[_len-1] = 0x00;
+              printf("%s  \r",szBuf);
+            }
+          }
+          break;          
+          default:
+            break;
+          }
+        }
         break;
+      case SDL_TEXTINPUT:
+      {
+        if(nInputFSM == 1)
+        {
+          strcat(szBuf,_event.text.text);
+          printf("%s  \r",szBuf);
+        }
+      }
+      break;
       case SDL_QUIT:
         bLoop = SDL_FALSE;
         break;
@@ -117,7 +188,7 @@ int main(int argc, char *argv[])
   SDL_DestroyTexture(g_pTileSet);
 
   tDE_closeCore(g_pEngineCore);
-  
+
   SDL_Quit();
   return 0;
 }
